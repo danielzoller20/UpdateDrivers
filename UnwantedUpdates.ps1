7569afe68d8b696163a8c0b61d4698af5998ada0
danielzoller <danielzoller20@gmail.com>
2025-05-31 11:24:14 +0200

    changed for livetest

--- UnwantedUpdates.ps1
+++ UnwantedUpdates.ps1
@@ -4,17 +4,15 @@
 
 # need to be identified by id, e.g. sp142036 (HP) or kbwl15rf (Lenovo)
 $UnwantedUpdatesByID = @(
-    "UnwantedSample", # Sample
-    "kbwl15rf", # WiFi-Driver for Lenovo 13w Yoga Gen2
-    "kbwl16rf" # another WiFi-Driver for Lenovo 13w Yoga Gen2
+    "UnwantedSampleID1", # Sample1
+    "UnwantedSampleID2" # Sample2
 )
 
 $UnwantedUpdatesByIDRegex = @(
-    "(?i)^kbwl\d{2}rf$",
-    "(?i)^kbwl\d{1}rf$"
+    "UnwantedSampleRegex1",
+    "UnwantedSampleRegex2"
 )
 
 $UnwantedUpdatesByDevice = @{
     "82YS" = {$_.Category -eq "Networking Wireless LAN"}
-    "HP ProBook x360 435 G8 Notebook PC" = {$_.Name -like "Realtek RTL8*"}
 }
