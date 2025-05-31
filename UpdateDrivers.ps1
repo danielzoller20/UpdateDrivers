8ea405cc9c51db498cf288a9098673809ef92891
danielzoller <danielzoller20@gmail.com>
2025-05-31 11:37:13 +0200

    Define unwanted Updates on GitHub

--- UpdateDrivers.ps1
+++ UpdateDrivers.ps1
@@ -54,7 +54,19 @@ function  Remove-UnwantedUpdates {
              $FinalFilteredUpdates = $IDFilteredUpdates
         }
 
-        $FinalFilteredUpdates
+
+        
+        $FinalIds = $FinalFilteredUpdates.Id
+        $UnwantedUpdates = $collectedUpdates | Where-Object { $_.Id -notin $FinalIds }
+
+        if ($UnwantedUpdates.Count -gt 0) {
+            Write-Host "Some updates are not applied"
+            $UnwantedUpdates
+        }
+
+        Write-Host "see $updateDefinitionsUrl for excluded updates"
+
+
     }
 
 }
@@ -76,22 +88,11 @@ Write-Host (Get-Date -Format $DateTimeFormat)-" script started"
 # unwanted updateds
 ###########################################################################
 
-# need to be identified by id, e.g. sp142036 (HP) or kbwl15rf (Lenovo)
-$UnwantedUpdatesByID = @(
-    "UnwantedSample", # Sample
-    "kbwl15rf", # WiFi-Driver for Lenovo 13w Yoga Gen2
-    "kbwl16rf" # another WiFi-Driver for Lenovo 13w Yoga Gen2
-)
 
-$UnwantedUpdatesByIDRegex = @(
-    "(?i)^kbwl\d{2}rf$",
-    "(?i)^kbwl\d{1}rf$"
-)
+$updateDefinitionsUrl = "https://raw.githubusercontent.com/danielzoller20/UpdateDrivers/576f6c241bccfa0a69c04d2badc7520df704944f/UnwantedUpdates.ps1"
 
-$UnwantedUpdatesByDevice = @{
-    "82YS" = {$_.Category -eq "Networking Wireless LAN"}
-    "HP ProBook x360 435 G8 Notebook PC" = {$_.Name -like "Realtek RTL8*"}
-}
+Invoke-Expression (Invoke-RestMethod -Uri $updateDefinitionsUrl) -Verbose
+
 
 
 ###########################################################################
@@ -103,13 +104,13 @@ $RequiredNuGetVersion = "2.8.5.201"
 
 # PackageProvider NuGet not found
 if ($null -eq $InstalledNuGuet) {
-    Write-Host (Get-Date -Format $DateTimeFormat)-" NuGet needs to be installed"
+    Write-Host "NuGet needs to be installed"
     try {
         Install-PackageProvider -Name NuGet -RequiredVersion $RequiredNuGetVersion -Force -ErrorAction Stop
-        Write-Host (Get-Date -Format $DateTimeFormat)-" NuGet installation sucessful"
+        Write-Host "NuGet installation sucessful"
     }
     catch {
-        Write-Host (Get-Date -Format $DateTimeFormat)-" NuGet could not be installed, script will exit!"
+        Write-Host "NuGet could not be installed, script will exit!"
         exit(1)
     }
 }
@@ -119,20 +120,20 @@ else {
     # try to get NuGet Version
     try {
         $NuGetVersion = ((Get-PackageProvider -name NuGet -Force -ErrorAction Stop).Version)
-        Write-Host (Get-Date -Format $DateTimeFormat)-" found or installed NuGet version $NuGetVersion" -ForegroundColor Green
+        Write-Host "found or installed NuGet version $NuGetVersion" -ForegroundColor Green
         }
     catch {
-        Write-Host (Get-Date -Format $DateTimeFormat)-" Something went wrong, will install NuGet $RequiredNuGetVersion or above"
+        Write-Host "Something went wrong, will install NuGet $RequiredNuGetVersion or above"
     }
     finally {
         # check for required NuGuet version
         if ($NuGetVersion -ge $RequiredNuGetVersion) {
-            Write-Host (Get-Date -Format $DateTimeFormat)-" current NuGet Version is greater or equal required version $RequiredNuGetVersion" -ForegroundColor Green
+            Write-Host "current NuGet Version is greater or equal required version $RequiredNuGetVersion" -ForegroundColor Green
         }
         else {
-            Write-Host (Get-Date -Format $DateTimeFormat)-" NuGet needs to be updated to $RequiredNuGetVersion or above"
+            Write-Host "NuGet needs to be updated to $RequiredNuGetVersion or above"
             Install-PackageProvider -Name NuGet -RequiredVersion $RequiredNuGetVersion -Force
-            Write-Host (Get-Date -Format $DateTimeFormat)-" NuGet installation sucessful"
+            Write-Host "NuGet installation sucessful"
         }
     }
 }
@@ -144,16 +145,16 @@ $DeviceModel = $ComputerInfo.CsModel
 
 # if device is from HP
 if ($DeviceManufacturer -eq "HP") {
-    Write-Host (Get-Date -Format $DateTimeFormat)-" This device seems to be manufactured by HP"
+    Write-Host "This device seems to be manufactured by HP"
     $UpdatePSModule = "HPDrivers"
     # $UpdatePrompt is executed to update all the drivers
     
     if ($BIOS -eq $true) {
-        Write-Host (Get-Date -Format $DateTimeFormat)-" Switch-Parameter to update BIOS was passed"
+        Write-Host "Switch-Parameter to update BIOS was passed"
         $UpdatePrompt = {Get-HPDrivers -NoPrompt -DeleteInstallationFiles -BIOS |  Remove-UnwantedUpdates}
     }
     else {
-        Write-Host (Get-Date -Format $DateTimeFormat)-" Switch-Parameter to update BIOS was not passed"
+        Write-Host "Switch-Parameter to update BIOS was not passed"
         $UpdatePrompt = {Get-HPDrivers -NoPrompt -DeleteInstallationFiles|  Remove-UnwantedUpdates}
     }
     
@@ -161,42 +162,42 @@ if ($DeviceManufacturer -eq "HP") {
 
 # if device is from Lenovo
 elseif ($DeviceManufacturer -eq "Lenovo") {
-    Write-Host (Get-Date -Format $DateTimeFormat)-" This device seems to be manufactured by Lenovo"
+    Write-Host "This device seems to be manufactured by Lenovo"
     $UpdatePSModule = "LSUClient"
     $UpdateSavePath = Join-Path -Path $RootDirectory -ChildPath "Drivers"
     # $UpdatePrompt is executed to update all the drivers
     $UpdatePrompt = {
         if ($BIOS -eq $true) {
-            Write-Host (Get-Date -Format $DateTimeFormat)-" Switch-Parameter to update BIOS was passed"
-            $Updates = Get-LSUpdate |  Remove-UnwantedUpdates
+            Write-Host "Switch-Parameter to update BIOS was passed"
+            $Updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended } | Remove-UnwantedUpdates
         }
         else {
-            Write-Host (Get-Date -Format $DateTimeFormat)-" Switch-Parameter to update BIOS was not passed"
-            $Updates = Get-LSUpdate | Where-Object {$_.Type -ne "BIOS"} |  Remove-UnwantedUpdates
+            Write-Host "Switch-Parameter to update BIOS was not passed"
+            $Updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended } | Where-Object {$_.Type -ne "BIOS"} |  Remove-UnwantedUpdates
         }
         
         if ($null -eq $Updates) {
-            Write-Host (Get-Date -Format $DateTimeFormat)-" No updates pending, script will exit"
+            Write-Host "No updates pending, script will exit"
         }
         else {
-            Write-Host (Get-Date -Format $DateTimeFormat)-" Drivers will be updated..." -ForegroundColor Yellow
-            Write-Host (Get-Date -Format $DateTimeFormat)-" found updates: $Updates"
+            Write-Host "Drivers will be updated..." -ForegroundColor Yellow
+            Write-Host "found updates: $Updates"
             New-Item -ItemType Directory -Path $UpdateSavePath -Force
             $Updates | Save-LSUpdate -Path $UpdateSavePath
             $Updates | Install-LSUpdate
             try {
                 Remove-Item -Path $UpdateSavePath -Force -Recurse -ErrorAction Stop
-                Write-Host (Get-Date -Format $DateTimeFormat)-" cleared temporary folder for driver-downloads" -ForegroundColor Green
+                Write-Host "cleared temporary folder for driver-downloads" -ForegroundColor Green
             }
             catch {
-                Write-Host (Get-Date -Format $DateTimeFormat)-" temporary folder for driver-downloads could not be cleared" -ForegroundColor Red
+                Write-Host "temporary folder for driver-downloads could not be cleared" -ForegroundColor Red
             }
         }
     }
 }
 
 else {
-    Write-Host (Get-Date -Format $DateTimeFormat)-" Device Manufacturer $DeviceManufacturer currently not supported"
+    Write-Host "Device Manufacturer $DeviceManufacturer currently not supported"
     Exit(1)
 }
 
@@ -204,22 +205,22 @@ else {
 # install module for updating drivers
 ###########################################################################
 
-Write-Host (Get-Date -Format $DateTimeFormat)-" checking for module $UpdatePSModule"
+Write-Host "checking for module $UpdatePSModule"
 
 # get version of module
 $ModuleVersion = (Get-Module -ListAvailable -Name $UpdatePSModule -ErrorAction Stop).Version
 
 # install module if version is null
 if ($null -eq $ModuleVersion) {
-    Write-Host (Get-Date -Format $DateTimeFormat)-" module not found, will be installed"
+    Write-Host "module not found, will be installed"
     Install-Module -Name $UpdatePSModule -Force
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
     Import-Module -Name $UpdatePSModule -Force
-    Write-Host (Get-Date -Format $DateTimeFormat)-" module installation sucessful"
+    Write-Host "module installation sucessful"
     
 }
 else {
-    Write-Host (Get-Date -Format $DateTimeFormat)-" found module $UpdatePSModule version $ModuleVersion" -ForegroundColor Green
+    Write-Host "found module $UpdatePSModule version $ModuleVersion" -ForegroundColor Green
 }
 
 
@@ -241,11 +242,11 @@ foreach ($FolderInC in $AllFoldersInC) {
     $FolderName = $FolderInC.Name
 
     if ($FolderName -match $RegexRemoveFolders -or $ExplicitRemoveFolders -contains $FolderName) {
-        Write-Host (Get-Date -Format $DateTimeFormat)-" $FolderName will be removed from C:\" -ForegroundColor Yellow
+        Write-Host "$FolderName will be removed from C:\" -ForegroundColor Yellow
         Remove-Item -Path $FolderInC.FullName -Recurse -Force
     }
     else {
-        Write-Host (Get-Date -Format $DateTimeFormat)-" $FolderName will stay on C:\"
+        Write-Host "$FolderName will stay on C:\"
     }
 }
 
